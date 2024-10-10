0819d2c637b6bf0cb107ec148b0f22f55b3f2d68
svetlana-1979 <svetlanasrurova@gmail.com>
2024-10-10 22:50:51 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,8 @@
+name = input('my_string ')
+q = len(name)
+print(name, "- количество символов ", q)
+print (name.upper())
+print(name.lower())
+print(name.replace(' ', ''))
+print (name[0])
+print(name[-1])
